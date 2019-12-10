35aa21a0f488fac99f7ad7402217d5f18a160e2c
ghostrib <sh4rkbot@gmail.com>
2019-12-10 16:15:19 +0100

    solution

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hovering: false
+    };
+    this.onListItemMouseEnter = this.onListItemMouseEnter.bind(this);
+    this.onListItemMouseLeave = this.onListItemMouseLeave.bind(this);
+  }
+
+  onListItemMouseEnter() {
+    this.setState({
+      hovering: true
+    });
+  }
+
+  onListItemMouseLeave() {
+    this.setState({
+      hovering: false
+    });
+  }
+
+  render() {
+    const style = {
+      fontWeight: this.state.hovering ? 'bold' : 'normal'
+    };
+
+    return (
+      <li
+        style={style}
+        onMouseLeave={this.onListItemMouseLeave}
+        onMouseEnter={this.onListItemMouseEnter}
+      >
+        {this.props.item}
+      </li>
+    );
+  }
+}
+
+const GroceryList = props => (
+  <ul>
+    {props.items.map(item => (
+      <GroceryListItem item={item} />
+    ))}
+  </ul>
+);
+
+ReactDOM.render(
+  <GroceryList items={['kale', 'cucumbers']} />,
+  document.getElementById('app')
+);
